73fb1c36e1f6f1dca42c2b5156a014522ce65e72
JohnnyHu9 <r17d09@gmail.com>
2015-03-10 10:02:45 -0400

    add ejs templaet

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Demo',
-                       name : 'Tom'
+                       name : 'Tom',
+                       firstHeadLine : 'Here is my first headline'
                       });
 });
 
