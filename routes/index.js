8b23e3dad121ba45008ac96cd68f4dd4a13a9166
JohnnyHu9 <r17d09@gmail.com>
2015-03-11 11:03:23 -0400

    update title

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Demo',
-                       name : 'Tom',
-                       firstHeadLine : 'Here is my first headline'
+                       name : 'ZhX Hu',
+                       firstHeadLine : 'Here is Zhx Hu first headline'
                       });
 });
 
